07e3f6991542cbdb6069c662fcd417fd12b01b5e
Linlew <24560270+linlew@users.noreply.github.com>
2023-12-08 19:07:37 -0600

    finished working on most of the guts

--- src/index.js
+++ src/index.js
@@ -1,9 +1,46 @@
 import './style.css';
 
+
+const domEditor = (function() {
+    let data = {};
+    let day = 0;
+
+    const locationDiv = document.querySelector('.location');
+    const conditionDiv = document.querySelector('.condition');
+    const temperatureDiv = document.querySelector('.temperature');
+    const iconDiv = document.querySelector('.weather-icon');
+
+    const updateValues = () => {
+        locationDiv.textContent = data.location;
+        conditionDiv.textContent = data.condition;
+        temperatureDiv.textContent = data.temp_f
+        iconDiv.src = data.icon;
+
+        day = data.is_day;
+    }
+
+    function updateData(dataObj) {
+        data = dataObj;
+        console.log(data);
+        updateValues();
+    }
+
+    const getData = () => {
+        console.log(data);
+    }
+
+    return{updateData, getData};
+
+})();
+
+
 async function getWeatherData(query) {
     try {
-        const response = await fetch('https://api.weatherapi.com/v1/current.json?key=ca6267a41fb3444db0b205826230712&q=' + query, {mode: 'cors'} );
+        // const response = await fetch('https://api.weatherapi.com/v1/current.json?key=ca6267a41fb3444db0b205826230712&q=' + query + '&days=3&aqi=no&alerts=yes', {mode: 'cors'} );
+        const response = await fetch('http://api.weatherapi.com/v1/forecast.json?key=ca6267a41fb3444db0b205826230712&q=' + query + '&days=3&aqi=no&alerts=yes', {mode: 'cors'} );
+
         const weatherData = await response.json();
+        console.log(weatherData);
         return weatherData;
         
     }
@@ -23,7 +60,11 @@ async function cleanWeatherData(query) {
             temp_f: weather.temp_f,
             temp_c: weather.temp_c,
             uv: weather.uv,
-            wind_dir: weather.wind_dir
+            wind_dir: weather.wind_dir,
+            wind_kph: weather.wind_kph,
+            wind_mph: weather.wind_mph,
+            humidity: weather.humidity,
+            is_day: weather.is_day
         }
         return weatherData;
     }
@@ -36,38 +77,21 @@ async function cleanWeatherData(query) {
 }
 
 
-async function addToDom(query) {
+async function storeWeatherData(query) {
     try {
-        const weather = await cleanWeatherData(query);
-        console.log(weather);
-
-        iconDiv.src = weather.icon;
-        locationDiv.innerHTML = "Location: " + weather.location;
-        weatherDiv.innerHTML = "Weather: " + weather.condition;
-        temperatureDiv.innerHTML = "Temperature: " + weather.temp_f;
-        
+        const cleanData = await cleanWeatherData(query)
+        domEditor.updateData(cleanData);
     }
     catch (error) {
-        console.log('error getting object')
+        console.log('error storing data');
     }
 }
 
-const searchBar = document.querySelector('#search-bar');
 
+const searchBar = document.querySelector('#search-bar');
 const searchBtn =document.querySelector('.search-btn');
 searchBtn.addEventListener('click', (event) => {
     event.preventDefault();
-    addToDom(searchBar.value);
-
+    storeWeatherData(searchBar.value);;
 })
-
-
-const locationDiv = document.querySelector('.location');
-const weatherDiv = document.querySelector('.weather');
-const temperatureDiv = document.querySelector('.temperature');
-const iconDiv = document.querySelector('.weather-icon');
-
-addToDom('50401');
-
-
 
